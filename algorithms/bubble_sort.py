de8f2cba6ca796817bcabaeb92f5e39fda98a014
Ellen <ellenmstuder@gmail.com>
2022-11-30 09:31:29 -0600

    fix bubble sort alg

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,29 +1,13 @@
-
 def bubble_sort(array):
     n = len(array)
-
     for i in range(n):
-        # print('array: ', array)       # THESE DON'T GET PRINTED
-        # print(f'array[i]: ', array[i])
-        for j in (0, n-i-1):
-            print('array: ', array)
-            print(f'array[i]: ', array[i])
-            print(f'array[j]: ', array[j])
+        for j in range(0, n-i-1):
             if array[j] > array[j+1]:
-                print(f'array[j+1]: ', array[j+1])
                 array[j], array[j+1] = array[j+1], array[j]
-                print(f'new array[j]: ', array[j])
-                print(f'new array[j+1]: ', array[j+1])
-                print('start over')
 
 
 a = [85, 42, 99, 15]
-
-# print(f'unsorted array: ', a)
+print('unsorted array: ', a)
 
 bubble_sort(a)
-# print(f'sorted array: ', a)
-
-
-# why is it jumping to the last array item (15/ 3) upon
-# the next iteration?
+print('sorted array: ', a)
