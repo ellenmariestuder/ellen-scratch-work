7529ba2b0c9eea2e9773f4e49ec3b7108f4500a0
Ellen <ellenmstuder@gmail.com>
2023-01-24 18:09:25 -0600

    reviewing selection sort

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,39 +1,88 @@
+arr = [5,7,4,2,1,9]
+
+size = len(arr)
 
 def selection_sort(array, size):
-    for i in range(size):
-        min_index = i    # space = constant
-        x = i*2          # space = c (so now 2c)
-        new_arr = array  # space = n (n refers to size of input array) 
-                         # new total space = n+c
-        # print(f'starting index: ', min_index)
-        # print(f'starting index value: ', array[i])
+    for i in range(0, size):
+        min_index = i
 
         for j in range(i+1, size):
-            # print(f'comparing index value: ', array[j])
             if array[j] < array[min_index]:
+
                 min_index = j
-                # print(f'new min index: ', min_index)
-            
+
         (array[i], array[min_index]) = (array[min_index], array[i])
 
+    return array
 
 
-a = [1,5,4,2,7]
-s = len(a)
-print(f'original array: ', a)
 
-selection_sort(a, s)
-print(f'sorted array: ', a)
 
-# space = 1c (1 x constant)
-# convention is to negate the constant (the 1). 
-# so space = c
 
-# asymptotic space complexity 
-# (asymptotic just means not discrete)
-# big O = O(1)
+print(f"input array: {arr}")
 
-# if it's just constants then it will be O(1)
-# doesn't matter how many constants-- will always be 1
+sorted_arr = selection_sort(arr, size)
 
-# (n[n+1])/2  <- arithmatic sequence proof
+print(f"sorted array: {sorted_arr}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(f"input array: {arr}")
+
+# def selection_sort(array):
+#     for i in range(len(array)):
+#         min_index = i
+
+#         for j in range(i+1, len(array)):
+#             if array[j] < array[min_index]:
+#                 min_index = j
+
+#         (array[i], array[min_index]) = (array[min_index], array[i])
+#     return array
+
+# arr2 = selection_sort(arr)
+
+# print(f"sorted array: {arr2}")
+
+
+# # WHAT ABOUT ARRAY OF HIGH MED LOW
